d425bedd8708755b055760df2acc201de439b692
ANarngAung <anarngaung2-17@gmail.com>
2024-12-24 14:28:15 +0630

    added rout in server js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // node_module တွင်ရှိ express ကို လှမ်းခေါ်ယူသုံးဖို့ရန် တည်ဆောက်ခြင်း
 const express = require('express');
 
+// server port နံပါတ် သတ်မှတ်ပေးခြင်း
+const port = 5070;
+
 // ခေါ်ယူထားသော express ကို နာမည် သတ်မှတ်ပေးခြင်း
 const cddk = express();
 
-// server port နံပါတ် သတ်မှတ်ပေးခြင်း
-const port = 5050;
+// Rout ကို တည်ဆောက်ခြင်း
+cddk.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 // console ထုတ်ပေးဖို့ရန်
 cddk.listen(port, () => console.log(`Server is running on http://localhost${port}`));
