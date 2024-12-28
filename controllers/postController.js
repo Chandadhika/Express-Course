35a2c558ec1f1a374f25d3325980bcd104f7bd94
ANarngAung <anarngaung2-17@gmail.com>
2024-12-28 18:27:00 +0630

    move to posts.js code to postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,14 @@
+// Object
+let posts = [
+    {id: 1, title: 'Post One'},
+    {id: 2, title: 'Post Two'},
+    {id: 3, title: 'Post Three'},
+];
+
+
 // @desc Get all posts
 // @route GET /api/posts
-
-const getPosts = (req, res, next) => {
+export const getPosts = (req, res, next) => {
     const limit = parseInt(req.query.limit);
 
     // နံပါတ်ရှိရင် ရှိတဲ့နံပါတ်က 0ထက် ကြီးရင် 
@@ -14,7 +21,7 @@ const getPosts = (req, res, next) => {
 
 // @desc single post
 // @desc /api/posts/:id
-const getPost = (req, res, next) => {
+export const getPost = (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
 
@@ -26,5 +33,48 @@ const getPost = (req, res, next) => {
         res.status(200).json(post);
  };
 
- // @desc single post
-// @desc /api/posts/:id
+ // @desc   Get new post
+// @desc    /api/posts/:id
+export const createPost = (req, res, next) => {
+    // console.log(req.body);
+    const newPost = {
+            id: posts.length + 1,
+            title: req.body.title,
+    };
+
+    if (!newPost.title) {
+        const error = new Error(`Please include a title`);
+       error.status = 404;
+       return next(error);
+    }
+    posts.push(newPost);
+    res.status(201).json(posts);
+};
+
+// @desc   Update post
+// @desc    /api/posts/:id
+export const updatePost =(req, res, next) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        return res.status(404)
+        .json({ msg: `A post with the id of ${id} was not found`});
+    }
+    post.title = req.body.title;
+    res.status(200).json(posts);
+};
+
+// @desc   Delete post
+// @desc    /api/posts/:id
+export const deletePost = (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        return res.status(404)
+        .json({ msg: `A post with the id of ${id} was not found`});
+    }
+    posts = posts.filter((post) => post.id !== id);
+    res.status(200).json(posts);
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,12 @@
 // const express = require('express');
 import express from 'express';
+import { 
+    getPosts,
+    getPost,
+    createPost,
+    updatePost,
+    deletePost,
+} from '../controllers/postController.js';
 
 const router = express.Router();
 
@@ -19,12 +26,7 @@ const router = express.Router();
 // });
 
 
-// Object
-let posts = [
-    {id: 1, title: 'Post One'},
-    {id: 2, title: 'Post Two'},
-    {id: 3, title: 'Post Three'},
-];
+
 
 // Get all post
 // query
@@ -35,16 +37,7 @@ let posts = [
 
 // limit
 // status(200)
-router.get('/', (req, res, next) => {
-    const limit = parseInt(req.query.limit);
-
-    // နံပါတ်ရှိရင် ရှိတဲ့နံပါတ်က 0ထက် ကြီးရင် 
-    if (!isNaN(limit) && limit > 0) {
-       return res.status(200).json(posts.slice(0, limit));
-    } else {
-        res.status(200).json(posts);
-    }
-});
+router.get('/', getPosts);
 
 // Get single post
 // filter
@@ -54,60 +47,17 @@ router.get('/', (req, res, next) => {
 //     });
 
 // find
-router.get('/:id', (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-
-    if (!post) {
-       const error = new Error(`A post with the id of ${id} was not found`);
-       error.status = 404;
-       return next(error);
-    } 
-        res.status(200).json(post);
- });
+// Get single post
+router.get('/:id', getPost);
 
 //  Create new post
-router.post('/', (req, res, next) => {
-    // console.log(req.body);
-    const newPost = {
-            id: posts.length + 1,
-            title: req.body.title,
-    };
-
-    if (!newPost.title) {
-        const error = new Error(`Please include a title`);
-       error.status = 404;
-       return next(error);
-    }
-    posts.push(newPost);
-    res.status(201).json(posts);
-});
+router.post('/', createPost);
 
 // Update Post
-router.put('/:id', (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-
-    if (!post) {
-        return res.status(404)
-        .json({ msg: `A post with the id of ${id} was not found`});
-    }
-    post.title = req.body.title;
-    res.status(200).json(posts);
-});
+router.put('/:id', updatePost);
 
 // Delete Post
-router.delete('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-
-    if (!post) {
-        return res.status(404)
-        .json({ msg: `A post with the id of ${id} was not found`});
-    }
-    posts = posts.filter((post) => post.id !== id);
-    res.status(200).json(posts);
-});
+router.delete('/:id', deletePost);
 
  export default router;
 
